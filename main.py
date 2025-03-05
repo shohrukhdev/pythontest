5dc9956d8cc7442114a443ee70cd528d4fe49ffe
shohrukhdev <shohrukhdev@gmail.com>
2025-03-05 14:58:35 +0500

    manual tests with local datda

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 from database import engine, Base
+from routes import router
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
+
 @app.get("/")
 def root():
     return {"message": "Construction Task Manager API for tsting"}
+
+
+app.include_router(router)
 
 
 @app.get("/hello/{name}")
