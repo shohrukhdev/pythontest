572e760425928af41766db8a4ff624882b7fb208
shohrukhdev <shohrukhdev@gmail.com>
2025-03-05 15:04:24 +0500

    added test db for unit tests updated reqs txt

--- database.py
+++ database.py
@@ -6,3 +6,14 @@ DATABASE_URL = "sqlite:///./construction_ai.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+
+TEST_DATABASE_URL = "sqlite:///./test_construction_ai.db"
+
+test_engine = create_engine(TEST_DATABASE_URL, connect_args={"check_same_thread": False})
+
+TestSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=test_engine)
+
+def init_test_db():
+    Base.metadata.drop_all(bind=test_engine)
+    Base.metadata.create_all(bind=test_engine)
